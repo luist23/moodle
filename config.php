e5d909512864407fef219da17b78dbc9360a740d
luist23 <37309985+luist23@users.noreply.github.com>
2022-03-10 15:08:33 -0600

    ...

--- config.php
+++ config.php
@@ -4,21 +4,20 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = 'pgsql';
-$CFG->dblibrary = 'native';
-$CFG->dbhost    = 'ec2-52-70-186-184.compute-1.amazonaws.com';
-$CFG->dbname    = 'd3hg1b60shlj9m';
-$CFG->dbuser    = 'fzemllrgwdehuh';
-$CFG->dbpass    = '474f465875cd57b4a5b4ed40bd80d56a20f72cc479f219ac9972311309fc601f';
+$CFG->dbtype    = getenv('DATABASE_TYPE');
+$CFG->dbhost    = getenv('DATABASE_HOST');
+$CFG->dbname    = getenv('DATABASE_NAME');
+$CFG->dbuser    = getenv('DATABASE_USER');
+$CFG->dbpass    = getenv('DATABASE_PASSWORD');
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
-  'dbport' => '5432',
+  'dbport' => getenv('DATABASE_PORT'),
   'dbsocket' => '',
 );
 
-$CFG->wwwroot   = 'https://moodletheme.herokuapp.com/, http not https';
-$CFG->dataroot  = 'try /tmp';
+$CFG->wwwroot   = getenv('WWWROOT');
+$CFG->dataroot  = getenv('DATAROOT');
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
